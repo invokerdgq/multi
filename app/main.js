41ce95cc76a5858b6673977b28bc5ae66355bd8b
daiguoqing <1586087329@qq.com>
2020-01-06 21:54:19 +0800

    muiti-2修改js

--- app/main.js
+++ app/main.js
@@ -1,2 +1,15 @@
-console.log('gggg')
+function test() {
+    let a = {
+        name:'少少',
+        age:18,
+        lvl:20,
+        wife:true
+    };
+   return Object.keys(a).reduce((pre,value) => {
+        pre.push(a[value]);
+        return pre
+    },[])
+}
+let data = test();
+console.log(data)
 
