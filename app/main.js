31e74c94e63c4155d28ca7c1c68eb22bcebcd827
daiguoqing <1586087329@qq.com>
2020-03-09 18:20:23 +0800

    fix bug

--- app/main.js
+++ app/main.js
@@ -7,5 +7,9 @@ console.log('gggg');
 console.log('gggg');
 console.log('yyyyyyyyyyyyyyyyy')
 console.log('fffffff')
-console.log('ttttttttttttttttttttttttttttttt')
+console.log('ttttttttttttttttttttttttttttttt');
+function Test() {
+    this.name = 'lll';
+    this.age = 18
+}
 
